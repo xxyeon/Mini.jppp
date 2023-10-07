73fd05923f4ed24b0d5a23f4561d21c04e36bf45
xxyeon <jykim9335@gmail.com>
2023-10-08 02:40:17 +0900

    css 수정 .word-preview 가로 가운데 정렬 수정

--- templates/js/learnJS.js
+++
@@ -1,8 +0,0 @@
-/*import bootstrap from "bootstrap";
-
-btn.addEventListner('click',function() {
-    var carouselExample = document.querySelector('#carouselExample');
-    var carousel = bootstrap.Carousel.getInstance(carouselExample); 
-    carousel.next();
-})
-*/
