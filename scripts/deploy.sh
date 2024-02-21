1ff1307a57c3696fd2101bef906eaf973cbd1a22
JiYeon Kim <87413158+xxyeon@users.noreply.github.com>
2024-02-22 01:23:46 +0900

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,7 +7,7 @@ EXECUTABLE_NAME=miniProj
 
 echo "> Build 파일 복사"
 
-cp $REPOSITORY/zip/*.jar $REPOSITORY
+cp $REPOSITORY/zip/build/libs/*.jar $REPOSITORY
 
 echo "> 현재 구동중인 애플리케이션 pid 확인"
 
@@ -27,7 +27,7 @@ fi
 
 echo "> 새 애플리케이션 배포"
 
-JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep 'SNAPSHOT.jar' | tail -n 1)
+JAR_NAME=$(ls -tr $REPOSITORY/*.jar | grep -v plain | tail -n 1)
 
 echo "> JAR Name: $JAR_NAME"
 
